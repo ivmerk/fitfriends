56ba4f23ff2d95135f6e11eb282beec573339473
ivmerk <ivan.e.merkulov@gmail.com>
2023-07-24 19:18:44 +0300

    starting front

---
+++ frontend/src/const.ts
@@ -0,0 +1,13 @@
+export enum AppRoute {
+  Main = '/',
+  Enter = '/enter',
+  SingIn = '/login',
+  Registration = '/registration',
+  TrainerRoom = '/traineroom',
+  ClientRoom = '/clientroom',
+  ClientsList = '/clientlist',
+  ClientCard = '/clientcard',
+  TrainerCard = '/trainercard',
+  TrainingsLis = '/trainingslist',
+  TrainingCard = '/trainingcard',
+}
