f33169dbefe39504986049b2bcc0dacf639c2689
ivmerk <ivan.e.merkulov@gmail.com>
2023-08-05 10:10:08 +0300

    registration navigation done

--- frontend/src/pages/registration-screen/registration-screen.tsx
+++ frontend/src/pages/registration-screen/registration-screen.tsx
@@ -1,13 +1,28 @@
 import BackgroundLogo from '../../components/backgroung-logo/background-logo';
 import RegistrationFormBegin from '../../components/registration-form-begin/registration-form-begin';
+import { useAppSelector } from '../../hooks';
+import { getUserCommon } from '../../store/user-process/selector';
+import { UserRole } from '../../types/user-role.enum';
+import QuestionnaireUser from '../../components/questionnaire-user/questionnaire-user';
+import QuestionnaireCoach from '../../components/questionnaire-coach/questionnaire-coach';
+
 
 function RegistrationScreen():JSX.Element{
+  const registrationStatus = useAppSelector(getUserCommon);
+
+
+  function RegistrationDetailsForm():JSX.Element{
+    return(
+      registrationStatus && (registrationStatus.userRole === UserRole.Client) ? <QuestionnaireUser/> : <QuestionnaireCoach/>
+    );}
   return(
     <>
       <BackgroundLogo/>
       <div className="popup-form popup-form--sign-up">
         <div className="popup-form__wrapper">
-          <RegistrationFormBegin/>
+          { !registrationStatus ?
+            <RegistrationFormBegin/>
+            : <RegistrationDetailsForm/>}
         </div>
       </div>
 
